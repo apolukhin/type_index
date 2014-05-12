e2ceb6cf36e45f80ad499eade43e1ff2e5f66d41
Antony Polukhin <antoshkka@gmail.com>
2014-05-12 20:11:51 +0400

    Log to std::cerr in ctti_print_name test and implicitly include ctti. Define BOOST_NO_RTTI for nortti test, so that even old compilers could compile tests

--- test/ctti_print_name.cpp
+++ test/ctti_print_name.cpp
@@ -8,12 +8,13 @@
 #define BOOST_TEST_MODULE type_index_ctti_test_module
 #include <boost/test/unit_test.hpp>
 
+#include <iostream>
 
 // This cpp file:
 // * tests BOOST_TYPE_INDEX_CTTI_USER_DEFINED_PARSING macro
 // * outputs full ctti name so that TypeIndex library could be adjust to new compiler without requesting regression tester's help
 #define BOOST_TYPE_INDEX_CTTI_USER_DEFINED_PARSING (0,0,false,"")
-#include <boost/type_index.hpp>
+#include <boost/type_index/ctti_type_index.hpp>
 
 namespace user_defined_namespace {
     class user_defined_class {};
@@ -22,9 +23,9 @@ namespace user_defined_namespace {
 BOOST_AUTO_TEST_CASE(ctti_print_full_string_helper_test)
 {
     using namespace boost::typeindex;
-    BOOST_TEST_CHECKPOINT("int: " << ctti_type_index::type_id<int>());
-    BOOST_TEST_CHECKPOINT("double: " << ctti_type_index::type_id<double>());
-    BOOST_TEST_CHECKPOINT("user_defined_namespace::user_defined_class: " << ctti_type_index::type_id<user_defined_namespace::user_defined_class>());
+    std::cerr << "int: " << ctti_type_index::type_id<int>() << '\n';
+    std::cerr << "double: " << ctti_type_index::type_id<double>() << '\n';
+    std::cerr << "user_defined_namespace::user_defined_class: " << ctti_type_index::type_id<user_defined_namespace::user_defined_class>() << '\n';
 
     BOOST_CHECK(true);
 }
